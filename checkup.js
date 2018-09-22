42e223371eda634abe830c88dfccca561ae43315
Tomer Raz <tomer@tomerway.com>
2018-09-22 18:48:17 +0300

    added % to tooltip

--- checkup.js
+++ checkup.js
@@ -1,19 +1,27 @@
-const timeSlider = document.getElementById('time-slider');
+const timeSlider = document.getElementById("time-slider");
 
 noUiSlider.create(timeSlider, {
-    start: [50],
-    range: {
-        'min': 0,
-        'max': 100
+  start: [50],
+  range: {
+    min: 0,
+    max: 100,
+  },
+  step: 5,
+  format: {
+    to: function(value) {
+      return value + "%";
     },
-    step: 5,
-    // Move handle on tap, bars are draggable
-    behaviour: 'tap-drag',
-    tooltips: true,
-    // Show a scale with the slider
-    pips: {
-        mode: 'steps',
-        stepped: true,
-        density: 4
-    }
+    from: function(value) {
+      return value.replace("%", "");
+    },
+  },
+  // Move handle on tap, bars are draggable
+  behaviour: "tap-drag",
+  tooltips: true,
+  // Show a scale with the slider
+  pips: {
+    mode: "steps",
+    stepped: true,
+    density: 4,
+  },
 });
